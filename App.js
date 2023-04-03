05044d2736428054f6d93852ca817e4325f99cc7
Elijah <elijah.ivanik@inbox.ru>
2023-04-03 16:42:58 +0300

    Updates

--- App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { TodoList } from "components/TodoList/TodoList";
-import "./scss/app.scss";
-function App() {
-    return React.createElement(TodoList, null);
-}
-export default App;
--- components/TodoList/TodoList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from "./TodoList.module.scss";
-export const TodoList = () => {
-    return (React.createElement("div", { className: styles.container },
-        React.createElement("div", { className: styles.header },
-            React.createElement("h1", null, "TODO List"))));
-};
