e5795d0a6a5f8cc222c1a9e89308072e97bd1e50
Elijah <elijah.ivanik@inbox.ru>
2023-04-04 11:12:47 +0300

    feat: implement Add design

--- src/components/TodoList/Header/Header.tsx
+++ src/components/TodoList/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Add } from "./Add/Add";
 import styles from "./Header.module.scss";
 
 export const Header = () => {
   return (
     <div className={styles.header}>
       <h1 className={styles.label}>TODO List</h1>
-      <button className={styles.add}>+</button>
+      <Add />
     </div>
   );
 };
