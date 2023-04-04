d23d3c125a1cab1b1537b52c8d4133a4a1916f23
Elijah <elijah.ivanik@inbox.ru>
2023-04-04 13:53:14 +0300

    refactor: remove tsconfig.node.json

--- src/hooks/useRedux.ts
+++ src/hooks/useRedux.ts
@@ -1,6 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "redux/store";
 
-type DispatchFunc = () => AppDispatch;
-export const useAppDispatch: DispatchFunc = useDispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
