d097e3ac6bde13cfcd54008767abd739dadf8d11
Elijah <elijah.ivanik@inbox.ru>
2023-04-05 13:31:41 +0300

    refactor: repalce deprecated extraReducers with builder

--- src/redux/slices/todoSlice.ts
+++ src/redux/slices/todoSlice.ts
@@ -12,36 +12,31 @@ const todoSlice = createSlice({
   name: 'todo',
   initialState,
   reducers: {},
-  extraReducers: {
-    [`${todoAPI.get.pending}`]: (state) => {
-      state.isLoading = true;
-    },
-    [`${todoAPI.get.fulfilled}`]: (state, action) => {
-      state.todos = action.payload;
-      state.isLoading = false;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(todoAPI.get.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(todoAPI.get.fulfilled, (state, action) => {
+        state.todos = action.payload;
+        state.isLoading = false;
+      })
 
-    [`${todoAPI.create.pending}`]: (state, action) => {
-      state.isUpdating[action.meta.arg.id] = true;
-    },
-    [`${todoAPI.create.fulfilled}`]: (state, action) => {
-      state.todos = action.payload.todos;
-      delete state.isUpdating[action.payload.currentTodo];
-    },
-    [`${todoAPI.create.rejected}`]: (_, action) => {
-      throw new Error(action.error.stack);
-    },
+      .addCase(todoAPI.create.pending, (state, action) => {
+        state.isUpdating[action.meta.arg.id] = true;
+      })
+      .addCase(todoAPI.create.fulfilled, (state, action) => {
+        state.todos = action.payload.todos;
+        delete state.isUpdating[action.payload.currentTodo];
+      })
 
-    [`${todoAPI.update.pending}`]: (state, action) => {
-      state.isUpdating[action.meta.arg.id] = true;
-    },
-    [`${todoAPI.update.fulfilled}`]: (state, action) => {
-      state.todos = action.payload.todos;
-      delete state.isUpdating[action.payload.currentTodo];
-    },
-    [`${todoAPI.update.rejected}`]: (_, action) => {
-      throw new Error(action.error.stack);
-    },
+      .addCase(todoAPI.update.pending, (state, action) => {
+        state.isUpdating[action.meta.arg.id] = true;
+      })
+      .addCase(todoAPI.update.fulfilled, (state, action) => {
+        state.todos = action.payload.todos;
+        delete state.isUpdating[action.payload.currentTodo];
+      });
   },
 });
 
